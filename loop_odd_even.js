ae79d0874259bf225c5e431439ec6f0d2db970fa
amrita.tiwari.saturncube <amrita.tiwari@saturncube.com>
2023-06-19 12:12:48 +0530

    added to loop logical question

---
+++ loop_odd_even.js
@@ -0,0 +1,8 @@
+// loop that iterates from 0 to 15. For each iteration, it checks if the current number is odd or even,
+for (let i = 0; i <= 15; i++) {
+  if (i % 2 == 0) {
+    console.log(i, "is even numbers->");
+  } else {
+    console.log(i, "is odd numbers->");
+  }
+}
